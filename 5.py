41360dc8a8aa9c85e939b9c1782b0d5da1062ace
ryotokuro <ryotokuro@gmail.com>
2019-12-19 15:20:00 +1100

    added new instructions and parameter modes support - writing tests

--- 5.py
+++ 5.py
@@ -12,8 +12,14 @@
 
 import os
 
+print("Welcome to the TEST diagnostic program")
+input("Please enter the ID of the system: ")
 
-file = open("2.txt")
+# diagnostic tests
+def test_modes():
+    return 0
+
+file = open("5.txt")
 contents = file.read()
 init = contents.split(',')
 init = list(map(int, init))
@@ -28,25 +34,49 @@ for noun in range(100):  # from 0 to 99 inclusive
         intcode = init[:]
         # print("\n",intcode)
         
-        for i in range(0, len(intcode()), 4):
-            # opcode 2: add parameter 1 and parameter 2 and save at position specified in parameter 3
-            if intcode[i] == 1:
-                intcode[intcode[i+3]] = intcode[intcode[i+1]] + intcode[intcode[i+2]]
+        for i in range(0, len(intcode()), num_instructions):
+            opcode = str(intcode[i][-2:])
+            
+            # mode: 0 - position, 1 - value
+            p1_mode = str(intcode[i][-3:-2])
+            p2_mode = str(intcode[i][-4:-3])
+
+            if p1_mode == '1' and p2_mode == '1':
+                p1 = intcode[i+1]
+                p2 = intcode[i+2]
+            elif p1_mode == '0' and p2_mode == '1':
+                p1 = intcode[intcode[i+1]]
+                p2 = intcode[i+2]
+            elif p1_mode == '1' and p2_mode == '0':
+                p1 = intcode[i+1]
+                p2 = intcode[intcode[i+2]]
+            else:  # both in position mode
+                p1 = intcode[intcode[i+1]]
+                p2 = intcode[intcode[i+2]]
+
+            # OPERATIONS
+            # opcode 1: add parameter 1 and parameter 2 and save at position specified in parameter 3
+            if opcode == '01':
+                intcode[intcode[i+3]] = p1 + p2
+                num_instructions = 4
                 
             # opcode 2: multiply parameter 1 and parameter 2 and save at position specified in parameter 3
-            elif intcode[i] == 2:
-                intcode[intcode[i+3]] = intcode[intcode[i+1]] * intcode[intcode[i+2]]
+            elif opcode == '02':
+                intcode[intcode[i+3]] = p1 * p2
+                num_instructions = 4
                 
             # opcode 3: save input at position specified in parameter
-            elif intcode[i] == 3:
+            elif opcode == '03':
                 intcode[intcode[i+1]] = int(input())
+                num_instructions = 2
                 
             # opcode 4: output value at only parameter
-            elif intcode[i] == 4:
+            elif opcode == '04':
                 print(intcode[intcode[i+1]])
+                num_instructions = 2
                 
             # opcode 99: halt
-            elif intcode[i] == 99:
+            elif opcode == '99':
                 break
 
         # output matches desired
