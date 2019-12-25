a79faf6b19b291378250a3de80081ac854ad7db6
ryotokuro <ryotokuro@gmail.com>
2019-12-25 14:44:00 +1100

    stuck on how to change heads and traverse level order

--- 6.py
+++ 6.py
@@ -14,11 +14,65 @@ graph = defaultdict(list)
 for i in nodes:
     graph[i[:i.find(')')]].append(i[i.find(')')+1:])
 
-print(graph.keys())
+# print(graph.keys())
 orbits = 0
-for k in graph.keys():
-    indirect = 
+level = 1
+
+head = 'COM'
+
+reset = head
+children = graph[head]
+for c in children:
+    print(c)
+    orbits += level
+
+head = children[0]
+
+"""
+while True:
+    print(level, ': ', head, sep='')  # print level and head
+    reset = head
+    children = graph[head]  # keep head in case we need to go back (multiple children)
+    print(children)  # list of children e.g. ['C', 'G']
+
+    # into the nodes
+    for child_index in range(len(children)):
+        print(child_index)  # node position in children list
+        
+        for c in children:  # for each child node
+            orbits += level  # add level
+
+        
+        # move
+        for i in children:
+            if not children:
+                head = reset
+            else:
+                head = children[child_index]
+ 
+        
+    level += 1
+print(orbits)
+"""
+"""
+while True:
+    print(level, ': ', curr, sep='')
+    for i in curr:  # go through items in the list
+        print(i)
+        orbits += level * len(i)
+    #curr = 
+    level += 1
+"""
+
+"""
+for key, val in graph.items():
+    print(val)
+    #for i in val:
+        #orbits += level * len(graph[k])
+    level += 1
 #print(graph)
+print(orbits)
+"""
 
 
 # if not the last letter in the list
