9008f7decf4937edfcf6e5ddd5028ab5f4018512
ryotokuro <ryotokuro@gmail.com>
2020-01-05 13:15:14 +1100

    figured out bfs (level order traversel), now stuck on adding level counter

--- 6.py
+++ 6.py
@@ -21,117 +21,19 @@ queue = []
 orbits = 0
 level = 1
 
-head = 'COM'
+queue.append('COM')
 
-for i in range(5):
-    children = graph[head]
-    queue = children[:]
+while queue:  # while queue is not empty
+    head = queue[0]
+    del queue[0]
 
-    while queue:
-        orbits += level
-        print(level, ':', head, "->", children)
+    if graph[head]:  # if there are children
+        for i in graph[head]:
+            queue.append(i)
+            level += 1
+    orbits += level
+    print(level, ':', head, "->", graph[head])
 
-    # to make 'C' the head, it's children[0]
-    # to make 'G' the head, it's children[1]
-    # level stays as 3
-        head = queue[0]
-        del queue[0]
-        children = graph[head]
-        
-    level += 1
-
-
-"""
-# print(graph.keys())
-orbits = 0
-level = 1
-
-head = 'COM'
-
-while True:
-    reset = head
-    children = graph[head]
-    print(children)
-    for i in range(len(children)):
-        for c in children:
-            print(c)
-            orbits += level
-        
-        head = children[i]
-    head = reset
-"""  
-
-"""
-while True:
-    print(level, ': ', head, sep='')  # print level and head
-    reset = head
-    children = graph[head]  # keep head in case we need to go back (multiple children)
-    print(children)  # list of children e.g. ['C', 'G']
-
-    # into the nodes
-    for child_index in range(len(children)):
-        print(child_index)  # node position in children list
-        
-        for c in children:  # for each child node
-            orbits += level  # add level
-
-        
-        # move
-        for i in children:
-            if not children:
-                head = reset
-            else:
-                head = children[child_index]
- 
-        
-    level += 1
-print(orbits)
-"""
-"""
-while True:
-    print(level, ': ', curr, sep='')
-    for i in curr:  # go through items in the list
-        print(i)
-        orbits += level * len(i)
-    #curr = 
-    level += 1
-"""
-
-"""
-for key, val in graph.items():
-    print(val)
-    #for i in val:
-        #orbits += level * len(graph[k])
-    level += 1
-#print(graph)
-print(orbits)
-"""
-
-
-# if not the last letter in the list
-# and already exists, then find and create new list
-
-# COM)B)C)D)E)F
-# COM)B)G)H
-# COM)B)C)D)I
-# COM)B)C)D)E)J)K)L
-
-
-# if i start from B, I need to add the length of the tree
-# e.g. removing com gives me 31, to which I then need 11
-# - the 11 then comes from adding the length of the tree (B->L)
-
-
-# given B)C
-# if self.data == orbit[:1]  # i.e. current node is the one to add onto
-# else:
-# reset to top node and traverse again
-
-# algorithm:
-# - define the tree structure
-# - COM is head
-# - going through the map, I will add the leaves respectively
-# - B to C, C to D, D to E...
-# - once I hit a branch that I'm not on, i'll reset (e.g. B to G)
-#  >> find B first then add G and continue
-
+# to make 'C' the head, it's children[0]
+# to make 'G' the head, it's children[1]
+# level stays as 3
